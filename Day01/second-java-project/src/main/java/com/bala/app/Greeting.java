6f12bb677ae918c21088b37b9179cbd24e4964c2
bala <balasaikrishna.gattineni@tcs.com>
2023-01-12 07:10:58 +0000

    day 01

---
+++ Day01/second-java-project/src/main/java/com/bala/app/Greeting.java
@@ -0,0 +1,16 @@
+package com.rama.app;
+
+public class Greeting {
+
+    public String SayWelcome(){
+        return "Welcome from greeting";
+    }
+
+    public String SayHello(){
+        return "Hello from greeting";
+    }
+
+    public String SayBye(){
+        return "Bye from greeting";
+    }
+}
