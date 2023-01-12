fbfd3b030c2b3165b73658cbf5e05cccfff5f056
bala <balasaikrishna.gattineni@tcs.com>
2023-01-12 15:18:34 +0000

    day 01

--- Day01/second-java-project/src/main/java/com/bala/app/App.java
+++ Day01/second-java-project/src/main/java/com/bala/app/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Greeting greeting = new Greeting();
+        String result= greeting.SayWelcome();
+        String result1= greeting.SayHello();
+        String result2= greeting.SayBye();
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }
---
+++ Day01/second-java-project/src/main/java/com/bala/app/Greeting.java
@@ -0,0 +1,14 @@
+package com.bala.app;
+
+public class Greeting {
+
+    public String SayWelcome(){
+        return "Welcome from greeting";
+    }
+    public String SayHello(){
+        return "Hello from greeting";
+    }
+    public String SayBye(){
+        return "Bye from greeting";
+    }
+}
