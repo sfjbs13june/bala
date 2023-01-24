a80a85cba04c8df62d511c9db25cbe765235c6fc
sfjbs <balasaikrishna.gattineni@tcs.com>
2023-01-24 05:34:19 +0000

    hospital

---
+++ Day08/student-app/src/main/java/com/bala/app/controller/StudentController.java
@@ -0,0 +1,40 @@
+package com.bala.app.controller;
+
+import com.bala.app.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/get/students")
+    public List<Student> getStudents(){
+        List listStudents= new ArrayList();
+        Student  student1= new Student("sai", "1", "10", "xyz");
+        Student  student2= new Student("sai", "1", "10", "xyz");
+        Student  student3= new Student("sai", "1", "10", "xyz");
+
+        listStudents.add(student1);
+        listStudents.add(student1);
+        listStudents.add(student1);
+        return  listStudents;
+    }
+
+    @GetMapping("/map/students")
+    public Map mapStudents(){
+        Map studentMap= new HashMap();
+        Student  student1= new Student("sai", "1", "10", "xyz");
+        Student  student2= new Student("sai", "1", "10", "xyz");
+        Student  student3= new Student("sai", "1", "10", "xyz");
+
+        studentMap.put(student1.getName(),student1);
+        studentMap.put(student2.getName(),student2);
+        studentMap.put(student3.getName(),student3);
+        return  studentMap;
+    }
+}
---
+++ Day08/student-app/src/main/java/com/bala/app/model/Student.java
@@ -0,0 +1,54 @@
+package com.bala.app.model;
+
+public class Student {
+    String name;
+    String roll;
+    String std;
+    String schoolname;
+
+
+
+    public Student() {
+
+    }
+
+    public Student(String name, String roll, String std,String schoolname) {
+        this.name = name;
+        this.roll = roll;
+        this.std = std;
+        this.schoolname = schoolname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public void setRoll(String roll) {
+        this.roll = roll;
+    }
+
+    public String getStd() {
+        return std;
+    }
+
+    public void setStd(String std) {
+        this.std = std;
+    }
+
+    public String getSchoolname() {
+        return schoolname;
+    }
+
+    public void setSchoolname(String schoolname) {
+        this.schoolname = schoolname;
+    }
+
+}
